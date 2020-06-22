3dd0c7c4767892fe2ed7f4ccc7211fb1c99aac9f
Mustafa <myilmazpakturk@gmail.com>
2020-06-21 20:29:06 -0400

    feature da ben commit yaptim

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		System.out.println("ilk commit from local repository");
 		System.out.println("ikinci commit from local repository");
 		System.out.println("yeni branch olusturdum ve adi feature");
+		System.out.println("bu ben yeni olusturdum feature branchda");
 
 	}
 
