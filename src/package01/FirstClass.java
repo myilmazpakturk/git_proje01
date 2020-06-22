86f991617931ff002bde64adf7071934ec650fc9
Mustafa <myilmazpakturk@gmail.com>
2020-06-21 20:34:09 -0400

    mustafa branch'da degisiklik yaptim

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,11 +5,12 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repository");
 		System.out.println("ikinci commit from local repository");
-		
+		System.out.println("mustafa branchda ben degisiklik yapiyorum");
 		System.out.println("master'dan israrci commit");
 
 		System.out.println("master dan yeni bir commit");
 		System.out.println("yeni olusturulan branch'dan commit");
+		
 	}
 
 }
