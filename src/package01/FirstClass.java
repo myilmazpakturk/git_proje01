5dcf425c2fa4eff499ee9f4eb5bfaecf509f4cdf
myilmazpakturk <67061931+myilmazpakturk@users.noreply.github.com>
2020-06-21 20:07:10 -0400

    Update FirstClass.java
    git hub dan commit var

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repository");
 		System.out.println("ikinci commit from local repository");
+		System.out.println("ilk commit from githup repository");
 
 	}
 
